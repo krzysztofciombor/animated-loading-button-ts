b157a2f039ecbca757f0e55793a0bb3403eaa1af
Krzysztof Ciombor <krzysztof.ciombor@swmansion.com>
2018-08-23 17:12:53 +0200

    Improve AnimatedLoadingButton animation

--- src/AnimatedLoadingButton.tsx
+++ src/AnimatedLoadingButton.tsx
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
-import { View } from "react-native";
-import posed from "react-native-pose";
+import {
+  View,
+  Animated,
+  LayoutChangeEvent,
+  Easing,
+  StyleSheet
+} from "react-native";
 
 interface Props {
   isLoading: boolean;
@@ -9,51 +14,135 @@ interface Props {
   block?: boolean;
 }
 
-type AnimationState = "default" | "loading";
+interface State {
+  isAnimating: boolean;
+}
 
-const buttonAnimConfig: { [key in AnimationState]: any } = {
-  default: { scaleX: 1, opacity: 1 },
-  loading: { scaleX: 0, opacity: 0 }
-};
+const ANIMATION_DURATION = 400;
 
-const loadingAnimConfig: { [key in AnimationState]: any } = {
-  default: { opacity: 0, scaleX: 0, scaleY: 0 },
-  loading: { opacity: 1, scaleX: 1, scaleY: 1 }
-};
+export default class AnimatedLoadingButton extends Component<Props, State> {
+  _animationState = new Animated.Value(0); // 0 - idle; 1 - loading
 
-const ButtonWrapper = posed.View(buttonAnimConfig);
+  _buttonHeight?: number = undefined;
+  _buttonWidth?: number = undefined;
 
-const LoadingWrapper = posed.View(loadingAnimConfig);
+  _loadingHeight?: number = undefined;
+  _loadingWidth?: number = undefined;
 
-export default class AnimatedLoadingButton extends Component<Props> {
+  state = {
+    isAnimating: false
+  };
+
+  componentWillReceiveProps(nextProps: Props) {
+    if (nextProps.isLoading !== this.props.isLoading) {
+      this._animateButton(nextProps.isLoading);
+    }
+  }
+
+  _animateButton = (isLoading: boolean) => {
+    this.setState({
+      isAnimating: true
+    });
+    Animated.timing(this._animationState, {
+      toValue: isLoading ? 1 : 0,
+      duration: ANIMATION_DURATION,
+      easing: Easing.inOut(Easing.quad)
+    }).start(() => {
+      this.setState({
+        isAnimating: false
+      });
+    });
+  };
+
+  onButtonLayout = (event: LayoutChangeEvent) => {
+    if (this._buttonHeight === undefined) {
+      this._buttonHeight = event.nativeEvent.layout.height;
+    }
+    if (this._buttonWidth === undefined) {
+      this._buttonWidth = event.nativeEvent.layout.width;
+    }
+  };
+
+  onLoadingLayout = (event: LayoutChangeEvent) => {
+    if (this._loadingHeight === undefined) {
+      this._loadingHeight = event.nativeEvent.layout.height;
+    }
+    if (this._loadingWidth === undefined) {
+      this._loadingWidth = event.nativeEvent.layout.width;
+    }
+  };
+
   render() {
-    const { isLoading, renderButton, renderLoading, block } = this.props;
-    const animationState: AnimationState = isLoading ? "loading" : "default";
+    const { renderButton, renderLoading, block } = this.props;
     return (
-      <View
-        style={{
-          alignItems: "center",
-          justifyContent: "center",
-          flex: 1,
-          flexDirection: "column"
-        }}
-      >
-        <View style={{ flexDirection: "row", flexGrow: 1 }}>
-          <ButtonWrapper
-            pose={animationState}
-            style={{ flexGrow: block ? 1 : 0 }}
+      <View style={styles.container}>
+        <Animated.View
+          style={{
+            flexGrow: block && !this.state.isAnimating ? 1 : 0,
+            overflow: "hidden",
+            width:
+              this._buttonWidth &&
+              this._animationState.interpolate({
+                inputRange: [0, 1],
+                outputRange: [this._buttonWidth, this._loadingWidth || 40]
+              }),
+            height:
+              this._buttonHeight &&
+              this._animationState.interpolate({
+                inputRange: [0, 1],
+                outputRange: [this._buttonHeight, this._loadingHeight || 40]
+              }),
+            borderRadius:
+              this._buttonHeight &&
+              this._animationState.interpolate({
+                inputRange: [0, 1],
+                outputRange: [0, this._buttonHeight]
+              }),
+            opacity: this._animationState.interpolate({
+              inputRange: [0, 0.5, 1],
+              outputRange: [1, 1, 0]
+            })
+          }}
+          onLayout={this.onButtonLayout}
+        >
+          {typeof renderButton === "function" ? renderButton() : renderButton}
+        </Animated.View>
+        <View style={styles.loadingContainer} pointerEvents="none">
+          <Animated.View
+            style={{
+              transform: [
+                {
+                  scale: this._animationState.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: [0, 1]
+                  })
+                }
+              ],
+              opacity: this._animationState.interpolate({
+                inputRange: [0, 0.5, 1],
+                outputRange: [0, 0, 1]
+              })
+            }}
+            onLayout={this.onLoadingLayout}
           >
-            {typeof renderButton === "function" ? renderButton() : renderButton}
-          </ButtonWrapper>
-        </View>
-        <View style={{ position: "absolute" }} pointerEvents="none">
-          <LoadingWrapper pose={animationState}>
             {typeof renderLoading === "function"
               ? renderLoading()
               : renderLoading}
-          </LoadingWrapper>
+          </Animated.View>
         </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  loadingContainer: {
+    position: "absolute"
+  }
+});
